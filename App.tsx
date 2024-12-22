9fc60ac42dd46645d4c297b94fee83a3b68ad45b
AdamGerthel <insats@gmail.com>
2024-12-22 21:41:55 +0100

    refactor: simplify example

--- App.tsx
+++ App.tsx
@@ -1,49 +1,44 @@
-import React, { useEffect, useState } from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity, Dimensions } from 'react-native';
-import { skiaImageCache } from './src/assets/skia-images';
-import { CardBackground } from './src/components/CardBackground';
+import React, { useState } from 'react'
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View, TouchableOpacity, Dimensions } from 'react-native'
+import { CardBackground } from './src/components/CardBackground'
 
 const deviceWidth = Dimensions.get('window').width
 
 export default function App() {
-  const [cacheHasBeenGenerated, setCacheHasBeenGenerated] = useState(false)
   const [dimensions, setDimensions] = useState({
     width: 250,
     height: 250
   })
 
-  useEffect(() => {
-    if (!cacheHasBeenGenerated) {
-      skiaImageCache.generateCache().then(() => {
-        setCacheHasBeenGenerated(true)
-      })
-    }
-  }, [cacheHasBeenGenerated])
-
   const resizeRandomly = () => {
     setDimensions({
-      width: Math.min(Math.floor(Math.random() * 500) + 100, deviceWidth - 20),
-      height: Math.min(Math.floor(Math.random() * 200) + 100, 300)
+      width: Math.floor(Math.min(Math.random() * 200 + 100, deviceWidth - 20)),
+      height: Math.floor(Math.min(Math.random() * 200 + 100, 300))
     })
   }
 
   return (
     <View style={styles.container}>
-      {!cacheHasBeenGenerated && <Text>Caching images...</Text>}
       <StatusBar style="auto" />
-      {cacheHasBeenGenerated && (
-        <>
-        <View style={styles.buttonContainer}>
-        <TouchableOpacity onPress={resizeRandomly} style={styles.button}><Text style={styles.buttonLabel}>Resize</Text></TouchableOpacity>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity onPress={resizeRandomly} style={styles.button}>
+          <Text style={styles.buttonLabel}>Resize</Text>
+        </TouchableOpacity>
+        <Text>
+          Canvas size: {dimensions.width}x{dimensions.height}
+        </Text>
       </View>
       <View style={styles.cardContainer}>
-      <CardBackground tint="#D77846" width={dimensions.width} height={dimensions.height} cornerSize={15} corners='all' />
+        <CardBackground
+          tint="#D77846"
+          width={dimensions.width}
+          height={dimensions.height}
+          radius={25}
+        />
       </View>
-        </>
-      )}
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -51,12 +46,12 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
   buttonContainer: {
     position: 'absolute',
     top: 80,
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
   button: {
     backgroundColor: 'blue',
@@ -66,9 +61,10 @@ const styles = StyleSheet.create({
     marginTop: 20
   },
   buttonLabel: {
-    color: 'white'
+    color: 'white',
+    textAlign: 'center'
   },
   cardContainer: {
     marginTop: 20
   }
-});
+})
